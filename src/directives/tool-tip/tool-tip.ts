b66292510cccd10f6a2448237d019ca08bc6f5ef
sclMAX <maxisoftman@gmail.com>
2017-08-23 06:49:26 -0300

    desarrollo

--- src/directives/tool-tip/tool-tip.ts
+++ src/directives/tool-tip/tool-tip.ts
@@ -1,28 +1,38 @@
-import {Toast, ToastController} from 'ionic-angular';
 import {Directive, HostListener, Input} from '@angular/core';
+import {Toast, ToastController} from 'ionic-angular';
 
+export let isHelpShow: boolean = false;
 @Directive({
   selector: '[tooltip]',  // Attribute selector
 })
 export class ToolTipDirective {
-  @Input() tooltip: string = '';
+  @Input() tooltip: string;
   helpToast: Toast;
+  isShow: boolean = false;
+
   constructor(private toastCtrl: ToastController) {}
 
   @HostListener('mouseenter')
   onMouseEnter() {
-    if (!this.helpToast) {
+    if (!isHelpShow) {
+      isHelpShow = true;
       this.helpToast =
-          this.toastCtrl.create({position: 'bottom', message: this.tooltip});
-      this.helpToast.present();
+          this.toastCtrl.create({position: 'bottom', message: this.tooltip,dismissOnPageChange:true});
+      this.helpToast.onDidDismiss(() => {
+        isHelpShow = false;
+        this.isShow = false;
+      });
+      this.helpToast.present().then(() => {this.isShow = true}).catch(() => {
+        this.isShow = false;
+        isHelpShow = false
+      });
     }
   }
 
   @HostListener('mouseleave')
   onMouseLeave() {
-    if (this.helpToast) {
+    if (isHelpShow && this.helpToast && this.isShow) {
       this.helpToast.dismiss();
-      this.helpToast = undefined;
     }
   }
 }
