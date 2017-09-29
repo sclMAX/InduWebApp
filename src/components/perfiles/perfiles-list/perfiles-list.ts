e63e4fb7bb3608bb75b9692f2cfc982f7f521f10
MAX <maxisoftman@gmail.com>
2017-09-29 09:24:22 -0300

    nada

--- src/components/perfiles/perfiles-list/perfiles-list.ts
+++ src/components/perfiles/perfiles-list/perfiles-list.ts
@@ -1,7 +1,7 @@
 import { Observable } from 'rxjs/Observable';
 import { ProductosPerfilesMppPage } from './../../../pages/productos/productos-perfiles-mpp/productos-perfiles-mpp';
 import { ModalController } from 'ionic-angular';
-import { Stock, StockItem } from './../../../models/stock.clases';
+import { StockItem } from './../../../models/stock.clases';
 import { StockProvider } from './../../../providers/stock/stock';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Perfil } from '../../../models/productos.clases';
@@ -14,35 +14,22 @@ export class PerfilesListComponent {
   @Input('showImg') showImg: boolean = true;
   @Input() showToolBar: boolean = true;
   @Output() onSelectItem: EventEmitter<Perfil> = new EventEmitter<Perfil>();
-  stock: Stock[] = [];
-  stocks: Array<{ id: string,totalBarras:number, stocks: Observable<StockItem[]> }> = [];
+  stocks: Array<{ id: string, totalBarras: number, stocks: Observable<StockItem[]> }> = [];
   @Output() onCalcTotalKilos: EventEmitter<number> = new EventEmitter<number>();
   constructor(private stockP: StockProvider, private modalCtrl: ModalController) { }
 
   onClickItem(perfil) { this.onSelectItem.emit(perfil); }
 
-  getStockTotalPerfil(idPerfil:string, items: StockItem[]): number {
+  getStockTotalPerfil(idPerfil: string, items: StockItem[]): number {
     let stockTotal: number = 0;
     if (items) {
       items.forEach((i) => { stockTotal += Number(i.stock); });
     }
-    let itemIdx = this.stocks.findIndex(i=>i.id == idPerfil);
-    if(itemIdx >-1){
+    let itemIdx = this.stocks.findIndex(i => i.id == idPerfil);
+    if (itemIdx > -1) {
       this.stocks[itemIdx].totalBarras = stockTotal;
     }
     return stockTotal;
-  }
-
-  getStocksPerfil(perfil: Perfil): StockItem[] {
-    let st = this.stock.find((s) => { return s.id == perfil.id; });
-    if (st && st.Stocks) {
-      return st.Stocks.sort((a, b) => {
-        if (a.id > b.id) return 1;
-        if (a.id < b.id) return -1;
-        return 0;
-      });
-    }
-    return [];
   }
 
   getStockPerfil(perfil: Perfil): Observable<StockItem[]> {
@@ -52,7 +39,7 @@ export class PerfilesListComponent {
     } else {
       this.stocks.push({
         id: perfil.id,
-        totalBarras:0,
+        totalBarras: 0,
         stocks: this.stockP.getOneStokcs(perfil.id)
       });
     }
@@ -73,21 +60,11 @@ export class PerfilesListComponent {
     return t;
   }
 
-  private async getItems(stk):Promise<StockItem[]>{
-    return new Promise<StockItem[]>((res,rej)=>{
-      stk.stocks.subscribe(s=>res(s),e=>rej(e));
-    });
-  }
-
   edit(item: Perfil) {
     let modal = this.modalCtrl.create(ProductosPerfilesMppPage, { Perfil: item });
     modal.present();
   }
 
-  ngOnInit() { this.getStock(); }
-  ionViewWillEnter() { this.getStock(); }
-
-  private async getStock() {
-    this.stockP.getAll().subscribe((data) => { this.stock = data; })
-  }
+  ngOnInit() { }
+  ionViewWillEnter() { }
 }
